7ffa2f81f81031bd5f1ae336c4fdbafde2a64112
anthemedge <d.mx.gibbs@gmail.com>
2016-05-14 14:31:46 +0100

    Colour Diagram Correction

--- matlab/ColourDiagrams/ColourMagnitudeGAMA.m
+++ matlab/ColourDiagrams/ColourMagnitudeGAMA.m
@@ -4,7 +4,7 @@ close
 %defined by cosmology
 h=0.7;
 %read in data
-data=csvread('GAMA/GAMA_colour.csv',1,0);
+data=csvread('GAMA_colour.csv',1,0);
 
 %Define columns and map redshift range
 gz=data(:,1);
@@ -21,24 +21,31 @@ stepsize=0.1;
 %setting up positions for plot
 x=[0.1 0.4 0.7 0.1 0.4 0.7];
 y=[0.5 0.5 0.5 0.1 0.1 0.1];
-figure1 = figure;
+
+ratio = 3;
+figPos = [100, 0, 297*ratio, 210*ratio];
+figure1 = figure('Position', figPos);
+
+dim = [.23 .6 .3 .3];
+
+
 for i=1:sz(2)
     
     
     %setting subplots with no gaps
     axes('Parent',figure1,...
         'Position',[x(:,i)-0.05 y(i) 0.3 0.4]);
-  
+    
     cMap=gz<=z(:,i)+stepsize & gz>=z(:,i) & r-5*log10(h)<-10;
     %apply map to data
     cutu=u(cMap);
     cutr=r(cMap);
-   
+    
     %generate values for plotting
     difference=cutu-cutr;
     X=cutr-5*log10(h);
     
-    dscatter(X, difference)
+    %dscatter(X, difference)
     colormap jet
     
     % caxis manual;
@@ -49,8 +56,22 @@ for i=1:sz(2)
     zl(i,:) =sprintf('%0.3f',z(:,i));
     zu(i,:) =sprintf('%0.3f',z(:,i)+stepsize);
     zsfull(i,:) =strcat(zl(i,:),'<z<', zu(i,:));
-    title(zsfull(i,:))
+    %title(zsfull(i,:))
     set(gca, 'box', 'on')
+    
+    
+    % Position of redshift text box
+    annotation('textbox',dim,'String',zsfull(i,:),'FitBoxToText','on');
+    if i == 3
+        dim = [.23 .2 .3 .3];
+    else
+        dim(1) = dim(1) + 0.299;
+    end
+    
+    
+    
+    
+    
     
     %remove ticks from non-edge plots
     if i== 2 || i== 3 || i==5 || i== 6
@@ -63,6 +84,21 @@ for i=1:sz(2)
     else
         xlabel('$$M_r-5logh$$','interpreter','latex')
     end
+    
+    %% avoid label overlaps
+    ax = gca;
+    if i == 4
+        ax.XTick = [-25, -20, -15];
+    elseif i == 6 ||  i == 5
+        ax.XTick = [-20, -15];
+    end
+    
+    if i <=3
+        ax.YTick = 0:0.5:3;
+    else
+        ax.YTick = 0:0.5:2.5;
+    end
+    
     
 end
 %colorbar;
